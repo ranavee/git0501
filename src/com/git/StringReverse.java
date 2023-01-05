5a1e5df91afdb4cf70c9067b38db93e1225dc366
ranavee <ranavee2022@gmail.com>
2023-01-05 16:25:28 +0530

    one

---
+++ src/com/git/StringReverse.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String s = "New Code";
+		String out = "";
+		String[] split = s.split(" ");
+		for (String a : split) {
+			String rev = "";
+			for (int i = a.length() - 1; i >= 0; i--) {
+				rev = rev + a.charAt(i);
+			}
+			System.out.print(out = rev + " ");
+		}
+	}
+}
